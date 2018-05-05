bc368bb266d73cdcfb95c9c91e2cf0795849888d
saporter_mac <scott.porter.ii@gmail.com>
2018-05-05 17:20:30 -0500

    Updated input manager

---
+++ Assets/Scripts/Drink.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drink : MonoBehaviour {
+    public int Lane = 0;
+    public float Whiskey = 0f;
+    public float Vodka = 0f;
+    public float Rum = 0f;
+    public float Soda = 0f;
+    public float Coke = 0f;
+    public float Vermouth = 0f;
+    public Garnish TypeOfGarnish = Garnish.Lime;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Down Lane: " + Lane + ". I am a drink of type (Whiskey, Vodka, Rum | Soda, Coke, Vermouth | Garnish): {" +
+                  Whiskey + ", " +
+                  Vodka + ", " +
+                  Rum + " | " +
+                  Soda + ", " +
+                  Coke + ", " +
+                  Vermouth + " | " +
+                  TypeOfGarnish +
+                  "}");
+	}
+	
+}
+
+public enum Garnish
+{
+    Lime, Cherry, Olive
+}
--- Assets/Scripts/KeyboardInputController.cs
+++ Assets/Scripts/KeyboardInputController.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyboardInputController : MonoBehaviour {
+    public Drink DrinkPrefab;
+    public float FillRate = .01f;
+    public int MaxLanes = 5;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    float whiskey, rum, vodka, soda, coke, vermouth;        // Final values
+    float iwhiskey, irum, ivodka, isoda, icoke, ivermouth;  // Input values per frame
+    int lane = 0;                                               // Current lane
 	
 	// Update is called once per frame
 	void Update () {
-		
+        GetLaneChangeInput();
+        GetDrinkMixingInput();
+
 	}
+
+    void GetLaneChangeInput()
+    {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            if (Input.GetAxis("Horizontal") < 0f)
+                lane = lane == 0 ? 0 : lane - 1;
+            else
+                lane = lane == MaxLanes ? MaxLanes : lane + 1;
+        }
+    }
+
+    void GetDrinkMixingInput()
+    {
+        iwhiskey = Input.GetAxis("Whiskey");
+        irum = Input.GetAxis("Rum");
+        ivodka = Input.GetAxis("Vodka");
+        isoda = Input.GetAxis("Soda");
+        icoke = Input.GetAxis("Coke");
+        ivermouth = Input.GetAxis("Vermouth");
+    }
 }
