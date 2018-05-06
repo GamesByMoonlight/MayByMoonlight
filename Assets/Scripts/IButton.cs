6f0d4601622242ec15b5b964e4a6ad9da144df2c
MikeDesktop <myantstew@gmail.com>
2018-05-06 17:46:23 -0500

    Basic buttons for mouse input. Test gameobjects

---
+++ Assets/Scripts/IButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public interface IButton
+{
+    void Clicked();
+
+    void Switch();
+
+    void Reset();
+}
