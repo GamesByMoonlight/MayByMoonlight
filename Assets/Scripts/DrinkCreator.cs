2c4be2b8c5257de6352d1c1be66670be1ced5476
ZaneEverett <36215239+Zaneeverett@users.noreply.github.com>
2018-05-06 16:41:09 -0500

    Adding in the bar prefabs with basic drink creation/destruction and slide physics. Also tag based

---
+++ Assets/Scripts/DrinkCreator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrinkCreator : MonoBehaviour {
+
+    public Rigidbody drinkPrefab;
+    public Transform drinkSpawnLoc;
+
+    bool keypress = false;
+
+	// Update is called once per frame
+	void Update () {
+
+        //Checking for keypress is a temporary work around until actual drink data can be passed in.
+        if (Input.GetKey("s") && !keypress)
+        {
+            Rigidbody drinkInstance;
+            drinkInstance = Instantiate(drinkPrefab, drinkSpawnLoc.position, drinkSpawnLoc.rotation) as Rigidbody;
+            drinkInstance.AddForce(500, 0, 0);
+            keypress = true;
+
+        }
+        else if (!Input.GetKey("s"))
+        {
+
+            keypress = false;
+        }
+		
+	}
+}
---
+++ Assets/Scripts/DrinkSlide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrinkSlide : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float xVel = 8000;
+
+    private Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+        // store the starting location for reset
+        //startPos = new Vector3(rb.transform.position.x, rb.transform.position.y, rb.transform.position.z);
+        //rb.AddForce(xVel, 0, 0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+}
