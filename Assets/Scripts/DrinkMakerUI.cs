5378549259aa0d60783f872d752d5a62bbd635cb
saporter_mac <scott.porter.ii@gmail.com>
2018-05-06 12:42:24 -0500

    Init UI and updated project settings

---
+++ Assets/Scripts/DrinkMakerUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrinkMakerUI : MonoBehaviour {
+    public GameObject DrinkToDisplay;
+
+    IMixedDrink theDrink;
+
+	// Use this for initialization
+	void Start () {
+        if(DrinkToDisplay != null)
+        {
+            theDrink = DrinkToDisplay.GetComponent<IMixedDrink>();
+            if (theDrink != null)
+                return;
+        }
+        Debug.Log("Could not find IMixedDrink implemented on DrinkToDisplay GameObject.  Can not display.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/IMixedDrink.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMixedDrink {
+
+    float Whiskey { get; }
+    float Rum { get; }
+    float Vodka { get; }
+    float Soda { get; }
+    float Coke { get; }
+    float Vermouth { get; }
+    int Lane { get; }
+    bool IsJustWater { get; } 
+}
