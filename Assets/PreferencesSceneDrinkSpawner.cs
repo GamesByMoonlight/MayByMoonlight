9237c19f9832b882811c245562b2d431b705baa3
Lance <lance.bailey@gmail.com>
2018-05-07 21:40:32 -0500

    * Created test scene for drink preferences * Test drink spawner created

---
+++ Assets/PreferencesSceneDrinkSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreferencesSceneDrinkSpawner : MonoBehaviour {
+
+    public GameObject drinkPrefab;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnDrink());
+	}
+
+
+    IEnumerator SpawnDrink()
+    {
+        yield return new WaitForSeconds(2);
+
+        Instantiate(drinkPrefab, transform);
+        StartCoroutine(SpawnDrink());
+    }
+
+}
