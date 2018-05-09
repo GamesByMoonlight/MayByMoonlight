e790b50bdf33c9ef8453e0005dd8bd855def2ef4
MikeDesktop <myantstew@gmail.com>
2018-05-09 01:17:45 -0500

    mouse input

---
+++ Assets/Scripts/scr_Button.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Button : MonoBehaviour
+{
+    public enum Type
+    {
+        rum,
+        vodka,
+        whiskey,
+        soda,
+        vermouth,
+        cola,
+        cherry,
+        lime,
+        olive,
+        water,
+        cancel
+    };
+
+    public scr_levelManager levelManager;
+    public Type myType;
+    private string myName;
+
+	// Use this for initialization
+	void Start () 
+    {
+        myName = myType.ToString();
+    }
+
+    public void PourDrink(string typeOfDrink)
+    {
+        if (typeOfDrink != levelManager.drinkType)
+            levelManager.drinkAmount = 0;
+        levelManager.drinkType = typeOfDrink;
+        levelManager.addDrink = true;
+    }
+
+    public void PourMixer(string typeOfMixer)
+    {
+        if (typeOfMixer != levelManager.mixerType)
+            levelManager.mixerAmount = 0;
+        levelManager.mixerType = typeOfMixer;
+        levelManager.addMixer = true;
+    }
+
+    public void StopPour()
+    {
+        levelManager.addDrink = false;
+        levelManager.addMixer = false;
+    }
+
+    public void Reset()
+    {
+        Debug.Log("reset");
+    }
+
+}
