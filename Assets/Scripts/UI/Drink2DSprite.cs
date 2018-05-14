e1b580b4b2b856f2d26d0f06d5a229ad4ff14410
Lance <lance.bailey@gmail.com>
2018-05-13 22:55:18 -0500

    * UI Images for drink making - now you're actually making a drink! * Replaced bars filling up with images of drinks filling up, with script references for wiring up with issue #38 and #39 * Note - currently there is no way to know what drink you're making.  That's temporary. * Will work on issue #38 next to restore functionality

---
+++ Assets/Scripts/UI/Drink2DSprite.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drink2DSprite : MonoBehaviour {
+
+    // Class to create a 2D image based on the values of IMixedDrink or IPatron
+    // Will search all components of a game object
+    
+    void Start () {
+        IPatron patron = transform.root.GetComponentInChildren<IPatron>();
+
+        if (patron != null)
+        {
+            Debug.Log("Do Patron logic");
+        }
+	}
+	
+    public void updateImage(IMixedDrink theDrink)
+    {
+        Debug.Log("Update UI Image");
+    }
+}
