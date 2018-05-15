36402c4e55cb37f0c1473c6ceb69ac5c36046c68
Lance <lance.bailey@gmail.com>
2018-05-15 15:19:33 -0500

    * Started the framework of a difficulty scaler * Considers game time, target game time as one set of paramaters * Displays on screen the current value for debug

---
+++ Assets/Scripts/DifficultyScaler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyScaler : MonoBehaviour {
+
+    public Text difficultyReadout;
+    
+    public float targetGameplaySeconds;
+
+    public int patronsOnscreen = 3;
+    public int maxSpecialPatrons = 1;
+
+    private float _calculatedDifficulty;
+    private float _timeMultiplier;
+
+    ScoreDisplay scoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+        scoreDisplay = FindObjectOfType<ScoreDisplay>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _timeMultiplier = Time.timeSinceLevelLoad > targetGameplaySeconds ? 1 : Time.timeSinceLevelLoad / targetGameplaySeconds;
+        _calculatedDifficulty = _timeMultiplier;
+
+
+        difficultyReadout.text = _calculatedDifficulty.ToString("#.0");
+	}
+}
