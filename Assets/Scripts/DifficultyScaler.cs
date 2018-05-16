c44382390e9a94742a466a4603ab56f93d391c87
Lance <lance.bailey@gmail.com>
2018-05-16 17:18:31 -0500

    * Finished first implementation of Difficulty Scaler * Needs wired to a spawner controller to confirm spawning of patrons

--- Assets/Scripts/DifficultyScaler.cs
+++ Assets/Scripts/DifficultyScaler.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 public class DifficultyScaler : MonoBehaviour {
 
     /* A little bit of math wizardry to try to start balancing # of patrons on screen with respect to player's performance, and target game time
+     * Patrons, SpecialPatrons are a reference for the spawners
      * 
+     * Formulas are just for testing purposes for now, should be tweaked based on feedback and when 
      * */
-
 
     private int _patrons = 1, _specialPatrons = 1;
 
     public int Patrons
     {
         get {
+            // Guarantee at least 1 patron always
             return Mathf.Clamp(_patrons, 1, 100);
         }
 
@@ -31,7 +33,8 @@ public class DifficultyScaler : MonoBehaviour {
     public Text patronsReadout;
     public Text specPatronsReadout;
 
-    public float targetGameplaySeconds;
+    // Poor performance during this time period should slow down gameplay accordingly (to guarantee at least a certain minimum play time)
+    public float targetGameplaySeconds = 60;
 
     public int patronsMultiplier = 3;
     public int specialPatronsMultiplier = 1;
@@ -41,7 +44,7 @@ public class DifficultyScaler : MonoBehaviour {
     private float buckLossAccumulator;
 
     private float _calculatedDifficulty;
-    private float _timeMultiplier;
+    private float _timeMultiplier = 1;
     
     private float _performanceIndicator;
 
@@ -56,10 +59,23 @@ public class DifficultyScaler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        _performanceIndicator = scoreDisplay.Bucks * 60 / (targetGameplaySeconds * bucksLossPerMinute);
+        // Performance indicator is how far away the player is from going broke in 30 seconds due to cash loss
+        // 1 means "going broke in 30 seconds", 2 means "going broke in 1 minute"
+        _performanceIndicator = scoreDisplay.Bucks / (2f * bucksLossPerMinute);
+        
+        float gameTime = Time.timeSinceLevelLoad;
 
-        _timeMultiplier = Time.timeSinceLevelLoad / targetGameplaySeconds;
-        _calculatedDifficulty = _timeMultiplier * _performanceIndicator;
+        if (targetGameplaySeconds != 0)
+                _timeMultiplier = (gameTime < targetGameplaySeconds) ? gameTime/targetGameplaySeconds : gameTime / 60;
+        
+        if (_timeMultiplier < 1 && _performanceIndicator < 1)
+        {
+            _calculatedDifficulty = _timeMultiplier;
+        }
+        else
+        {
+            _calculatedDifficulty = _timeMultiplier * _performanceIndicator;
+        }
 
         difficultyReadout.text = _calculatedDifficulty.ToString("#.0");
 
