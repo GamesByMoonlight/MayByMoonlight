2936f3e3786d15d7bc64e85ffea405c40f29ab6b
Lance <lance.bailey@gmail.com>
2018-05-16 15:23:28 -0500

    * Implemented output of patrons, special patrons for spawner controller * Refactored score display * Started baseline calculations to determine balance

--- Assets/Scripts/DifficultyScaler.cs
+++ Assets/Scripts/DifficultyScaler.cs
@@ -5,29 +5,73 @@ using UnityEngine.UI;
 
 public class DifficultyScaler : MonoBehaviour {
 
-    public Text difficultyReadout;
+    private int _patrons = 1, _specialPatrons = 1;
+
+    public int Patrons
+    {
+        get {
+            return Mathf.Clamp(_patrons, 1, 100);
+        }
+
+    }
+
+    public int SpecialPatrons {
+        get {
+            return _specialPatrons;
+        }
+
+    }
     
+    public Text difficultyReadout;
+    public Text patronsReadout;
+    public Text specPatronsReadout;
+
     public float targetGameplaySeconds;
 
-    public int patronsOnscreen = 3;
-    public int maxSpecialPatrons = 1;
+    public int patronsMultiplier = 3;
+    public int specialPatronsMultiplier = 1;
+
+    public int startingBucks = 100;
+    public int bucksLossPerMinute = 500;
+    private float buckLossAccumulator;
 
     private float _calculatedDifficulty;
     private float _timeMultiplier;
+    
+    private float _performanceIndicator;
+
 
     ScoreDisplay scoreDisplay;
 
 	// Use this for initialization
 	void Start () {
         scoreDisplay = FindObjectOfType<ScoreDisplay>();
+        scoreDisplay.Bucks = startingBucks;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        _timeMultiplier = Time.timeSinceLevelLoad > targetGameplaySeconds ? 1 : Time.timeSinceLevelLoad / targetGameplaySeconds;
-        _calculatedDifficulty = _timeMultiplier;
+        _performanceIndicator = scoreDisplay.Bucks * 60 / (targetGameplaySeconds * bucksLossPerMinute);
 
+        _timeMultiplier = Time.timeSinceLevelLoad / targetGameplaySeconds;
+        _calculatedDifficulty = _timeMultiplier * _performanceIndicator;
 
         difficultyReadout.text = _calculatedDifficulty.ToString("#.0");
-	}
+
+        _patrons = Mathf.RoundToInt(patronsMultiplier * _calculatedDifficulty);
+        patronsReadout.text = ("Patrons: " + Patrons);
+
+        _specialPatrons = Mathf.RoundToInt(specialPatronsMultiplier * _calculatedDifficulty);
+        specPatronsReadout.text = ("S. Patrons: " + SpecialPatrons);
+
+
+        buckLossAccumulator += bucksLossPerMinute / 60 * Time.deltaTime;
+        if (Mathf.RoundToInt(buckLossAccumulator) > 1)
+        {
+            scoreDisplay.SubtractBucks(Mathf.RoundToInt(buckLossAccumulator));
+            buckLossAccumulator -= Mathf.RoundToInt(buckLossAccumulator);
+        }
+
+
+    }
 }
--- Assets/Scripts/UI/ScoreDisplay.cs
+++ Assets/Scripts/UI/ScoreDisplay.cs
@@ -5,39 +5,25 @@ using UnityEngine.UI;
 
 public class ScoreDisplay : MonoBehaviour {
 
-    // This class is temporary just so that there's a UI on screen.  
-
-    public int bucks;
-    public int score;
+    public int Bucks { get; set; }
+    public int Score { get; set; }
 
     public Text bucksText;
     public Text scoreText;
-
-    public int bucksLossPerMinute = 500;
-
-    private float buckLossAccumulator = 0;
-
+    
 	public void UpdateScoreboard(DrinkScore drinkScore)
     {
-        bucks += drinkScore.Bucks;
-        score += drinkScore.Score;
+        Bucks += drinkScore.Bucks;
+        Score += drinkScore.Score;
 
-        bucksText.text = bucks.ToString();
-        scoreText.text = score.ToString();
+        bucksText.text = Bucks.ToString();
+        scoreText.text = Score.ToString();
     }
 
-    void Update()
+    public void SubtractBucks(int bucksToSubtract)
     {
-        buckLossAccumulator += bucksLossPerMinute / 60 * Time.deltaTime;
-
-        if (Mathf.RoundToInt(buckLossAccumulator) > 1)
-        {
-            bucks -= Mathf.RoundToInt(buckLossAccumulator);
-            buckLossAccumulator -= Mathf.RoundToInt(buckLossAccumulator);
-            bucksText.text = bucks.ToString();
-        }
-        
+        Bucks -= bucksToSubtract;
+        bucksText.text = Bucks.ToString();
     }
-
 
 }
