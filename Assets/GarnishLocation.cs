073165d2c644221e9c55d5803115d69d272b7758
Lance Bailey <37199007+LanceBailey@users.noreply.github.com>
2018-05-22 19:43:22 -0500

    Feature/41 - 3D drinks are being served! (#69)
    * * Added 3D glasses for drinks
    * 3D glasses have white fluid objects childed to them - these objects should have the textures/color applied to them based on primary mixer type
    * Also they have a location for the garnish on the rim of the glass (to be created)
    
    * * Working on 3D objects and scripts to make drinks appear correctly
    
    * The drinks now have their own glasses!  Work should be done on scaling things propery and the liquid inside seems a bit unnecessary but it works goddamit.

---
+++ Assets/GarnishLocation.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarnishLocation : MonoBehaviour {
+
+	// This is just so that a separate script can reference this component
+    // Think of this as a tag
+}
