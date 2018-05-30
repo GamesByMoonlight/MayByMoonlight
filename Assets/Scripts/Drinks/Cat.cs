61ac60130cd5b78d730a62771c2ae899ac7fb7b1
saporter <scott@gamesbymoonlight.com>
2018-05-30 16:14:39 -0500

    Cat swat drink animation

---
+++ Assets/Scripts/Drinks/Cat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour {
+    public float LateralBounceNoise = 5f;
+    public Animator Anim;
+
+    float lastTriggerTime;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<IMixedDrink>() != null)
+        {
+            collision.gameObject.GetComponent<Rigidbody>().velocity += new Vector3(0f, 0f, Random.Range(-LateralBounceNoise, LateralBounceNoise));
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<IMixedDrink>() == null)
+            return;
+        
+        if(Time.time - lastTriggerTime > .1f)
+        {
+            lastTriggerTime = Time.time;
+            Anim.SetTrigger("DrinkInbound");
+        }
+    }
+}
