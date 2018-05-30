4c3995129181b05479526b22f20ebbbb6e39788f
saporter <scott@gamesbymoonlight.com>
2018-05-30 15:39:43 -0500

    Bounciness on Cat.  Mesh colliders off in scene

---
+++ Assets/Scripts/Drinks/NoiseOnBounce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseOnBounce : MonoBehaviour {
+    public float LateralBounceNoise = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<IMixedDrink>() != null)
+        {
+            var rb = collision.gameObject.GetComponent<Rigidbody>().velocity += new Vector3(0f, 0f, Random.Range(-LateralBounceNoise, LateralBounceNoise));;
+        }
+
+    }
+}
