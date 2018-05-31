b4903e0f30b48a6090ff51411f62aea7a5c7fdde
Scott Porter <scott@gamesbymoonlight.com>
2018-05-31 15:11:06 -0500

    Feature/46 - Clean UI (#89)
    * Click table to make drink
    
    * Modified Drink Mixer UI
    
    * Score UI updated

---
+++ Assets/Scripts/UI/ClickToMakeDrink.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToMakeDrink : MonoBehaviour {
+    public scr_levelManager LevelManager;
+    public int Lane;
+
+    private void OnMouseDown()
+    {
+        LevelManager.MakeDrinkAtLane(Lane);
+    }
+}
